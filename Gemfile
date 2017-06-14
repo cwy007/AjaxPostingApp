9c26d4a71b966e34eaa07b6737dd934bb5943cb6
chanweiyan <chanweiyan007@gmail.com>
2017-06-14 18:41:58 +0800

    implement like function

--- Gemfile
+++ Gemfile
@@ -56,6 +56,8 @@ group :development do
 
   gem 'awesome_rails_console'
 
+  gem 'rails-erd'
+
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
 
+  has_many :likes, :dependent => :destroy
+  has_many :liked_posts, :through => :likes, :source => :post
+
   def display_name
     self.email.split("@").first
-  end 
+  end
 end
